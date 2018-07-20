d5edc469f35e1b8596d0ecdd1b69a05b6b6d84b7
rrivera <rllavona13@me.com>
2018-07-20 16:27:18 -0400

    edit

---
+++ Network_Tool_Web/PYEZ_Block_Attackers_GUI/blockAttackersGUI.py
@@ -0,0 +1,27 @@
+from Tkinter import *
+from jnpr.junos.utils.config import Config
+import json
+import mysql.connector
+
+form = Tk()
+
+topFrame = Frame(form)
+topFrame.pack()
+
+bottomFrame = Frame(form)
+bottomFrame.pack(side=BOTTOM)
+
+label1 = Label(topFrame, text="BLOCK IP ATTACKERS TOOL")
+label1.pack(fill=X)
+
+button1 = Button(bottomFrame, text="Block IP")
+button2 = Button(bottomFrame, text='Delete IP')
+button3 = Button(bottomFrame, text='Exit')
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=RIGHT)
+
+
+form.mainloop()
+
---
+++ Network_Tool_Web/Pyray_API/main.py
@@ -0,0 +1,6 @@
+from Tkinter import *
+
+form = Tk()
+label = Label(form, text='Block Attackers')
+label.pack()
+form.mainloop()

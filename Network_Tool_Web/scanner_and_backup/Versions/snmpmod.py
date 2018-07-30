f61ef810b5a541f6fb26e9a0e7710209f174fcfb
rllavona <rllavona13@me.com>
2018-07-29 21:43:37 -0400

    Edit to classes

--- Network_Tool_Web/scanner_and_backup/Versions/snmpmod.py
+++ Network_Tool_Web/scanner_and_backup/Versions/snmpmod.py
@@ -3,17 +3,35 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 class SnmpGet:
 
-    def __init__(self, host, oid):
+    def __init__(self, host, oid, community):
 
         self.host = host
         self.oid = oid
+        self.community = community
 
         snmp_gen = cmdgen.CommandGenerator()
 
         errorindication, errorstatus, errorindex, varbinds = snmp_gen.getCmd(
-            cmdgen.CommunityData('publ1c'),
-            cmdgen.UdpTransportTarget(((host), 161)), self.oid)
+            cmdgen.CommunityData(self.community),
+            cmdgen.UdpTransportTarget(((self.host), 161)), self.oid)
 
         for name, val in varbinds:
             oid = val
             print(str(oid))
+
+    def version(self, host, oid, community):
+
+        self.host = host
+        self.oid = oid
+        self.community = community
+
+        snmp_gen = cmdgen.CommandGenerator()
+
+        errorindication, errorstatus, errorindex, varbinds = snmp_gen.getCmd(
+            cmdgen.CommunityData(self.community),
+            cmdgen.UdpTransportTarget(((self.host), 161)), self.oid)
+
+        for name, val in varbinds:
+            oid = val
+            print(str(oid))
+
--- Network_Tool_Web/scanner_and_backup/Versions/testing.py
+++ Network_Tool_Web/scanner_and_backup/Versions/testing.py
@@ -1,32 +1,33 @@
 from snmpmod import SnmpGet
 
 
-hosts = '172.31.240.138 '
+hosts = '172.31.240.138'
 
 mikrotik_identity = 'iso.3.6.1.2.1.1.5.0'
 mikrotik_version = 'iso.3.6.1.2.1.47.1.1.1.1.2.65536'
 mikrotik_model = 'iso.3.6.1.2.1.1.1.0'
 mikrotik_serial = '1.3.6.1.4.1.14988.1.1.7.3.0'
+snmp_community = 'publ1c'
 
 
 def get_mk_name():
-    SnmpGet(host=hosts, oid=mikrotik_identity)
+    SnmpGet(host=hosts, oid=mikrotik_identity, community=snmp_community)
 
 
 def get_mk_version():
-    SnmpGet(host=hosts, oid=mikrotik_version)
+    SnmpGet.version(host=hosts, oid=mikrotik_version, community=mikrotik_version)
 
-
+"""
 def get_mk_model():
-    SnmpGet(host=hosts, oid=mikrotik_model)
+    SnmpGet(host=hosts, oid=mikrotik_model, community=snmp_community)
 
 
 def get_mk_serial():
-    SnmpGet(host=hosts, oid=mikrotik_serial)
-
+    SnmpGet(host=hosts, oid=mikrotik_serial, community=snmp_community)
+"""
 
 if __name__ == '__main__':
-    get_mk_name()
-    get_mk_model()
+   # get_mk_name()
+   # get_mk_model()
     get_mk_version()
-    get_mk_serial()
+    #get_mk_serial()

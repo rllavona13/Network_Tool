9b8e56958c5fd9d541bbfdde8bf173c81fea1e09
rrivera <rllavona13@me.com>
2018-08-17 15:49:11 -0400

    New Algorithm for nmap and SSH

--- Network_Tool_Web/Test/SSH.py
+++ Network_Tool_Web/Test/SSH.py
@@ -1,6 +1,7 @@
 import paramiko
 import json
 import nmap
+import mysql.connector
 
 config_file = open('auth.json')
 config = json.load(config_file)
@@ -39,19 +40,31 @@ class Scanner:
                             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                             ssh.connect(hostname=mk_list, username=config['username'], password=config['password'])
                             ssh.invoke_shell()
-                            stdin, stdout, stderr = ssh.exec_command('system identity print\n' 
-                                                                     'system routerboard print\n' 
-                                                                     'ip address print')
-
-                            fk_list.append(stdout.read().split('\r\n'))
-                            print(fk_list[0][0])
-                            # print(stdout.read())
+                            stdin, stdout, stderr = ssh.exec_command('system identity print')
+                            print(stdout.read())
 
                             print("==============================================================================")
                             ssh.close()
 
+                            sql_connector = mysql.connector.connect(user='python',
+                                                                    password='yzh8RB0Bcw1VivO3',
+                                                                    host='localhost',
+                                                                    database='test')
+
+                            cursor = sql_connector.cursor()
+
+                            add_mikrotik = ("INSERT INTO devices"
+                                            "(name, ip)"
+                                            "VALUES ('%s', '%s')" % (stdout.read(), mk_list))
+
+                            cursor.execute(add_mikrotik)
+                            sql_connector.commit()
+                            cursor.close()
+                            sql_connector.close()
+
                         except Exception as ex:  # print the error and continues with the next ip address
                             print(ex)
+
 
 if __name__ == '__main__':
     Scanner(host='172.31.240.0/24')

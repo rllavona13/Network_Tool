393bc473994abcffa08d9e4872ab73c9fcdef5c4
rrivera <rllavona13@me.com>
2018-08-20 12:50:33 -0400

    New Algorithm for nmap and SSH

--- Network_Tool_Web/Test/SSH.py
+++ Network_Tool_Web/Test/SSH.py
@@ -2,6 +2,8 @@ import paramiko
 import json
 import nmap
 import mysql.connector
+import sys
+from multiprocessing import Process
 
 config_file = open('auth.json')
 config = json.load(config_file)
@@ -15,9 +17,7 @@ class Scanner:
 
         self.host = host
         self.nmscanner = nmap.PortScanner()
-        self.nmscanner.scan(hosts=host, arguments='-Pn -p 8291')
-
-        fk_list = []
+        self.nmscanner.scan(hosts=host, arguments='-Pn -p 8291 --min-hostgroup 256 --ttl 10 --max-retries 1')
 
         for host in self.nmscanner.all_hosts():
 
@@ -41,6 +41,8 @@ class Scanner:
                             ssh.connect(hostname=mk_list, username=config['username'], password=config['password'])
                             ssh.invoke_shell()
                             stdin, stdout, stderr = ssh.exec_command('system identity print')
+
+                            mk_scanned_host = stdout.read()  # saves the output from ssh for MySQL query use
                             print(stdout.read())
 
                             print("==============================================================================")
@@ -55,7 +57,7 @@ class Scanner:
 
                             add_mikrotik = ("INSERT INTO devices"
                                             "(name, ip)"
-                                            "VALUES ('%s', '%s')" % (stdout.read(), mk_list))
+                                            "VALUES ('%s', '%s')" % (mk_scanned_host, mk_list))
 
                             cursor.execute(add_mikrotik)
                             sql_connector.commit()
@@ -67,4 +69,5 @@ class Scanner:
 
 
 if __name__ == '__main__':
-    Scanner(host='172.31.240.0/24')
+    Scanner(host='172.16.240.0/24')
+    # Scanner(host=sys.argv[1])

50e5cecd1864c569a09ff2987b00d30beb50a181
rllavona13@me.com <r4ym0nd12@>
2018-08-31 21:27:17 -0400

    edits

--- Network_Tool_Web/NEOLog/neo_log_server.py
+++ Network_Tool_Web/NEOLog/neo_log_server.py
@@ -4,3 +4,19 @@ import sys
 import json
 
 app = Flask(__name__)
+
+config_file = open('auth.json')
+config = json.load(config_file)
+config_file.close()
+
+
+def get_home():
+    pass
+
+
+def add_task():
+    pass
+
+
+def main_function():
+    pass

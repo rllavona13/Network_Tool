1fe597d6a4e3a80cea5a2d11300e6d385a579197
rllavona13@me.com <r4ym0nd12@>
2018-11-01 19:41:59 -0400

    New script

--- Network_Tool_Web/junos_software_upgrader/upgrader.py
+++ Network_Tool_Web/junos_software_upgrader/upgrader.py
@@ -7,7 +7,6 @@ Script by Ramon Rivera
 
 from jnpr.junos import Device
 from jnpr.junos.utils.sw import SW
-from pprint import pprint
 
 pkg = '/home/rrivera/scripts/junos_scripts/software_update/EX4200/jinstall-ex-4200-15.1R6-S2.1-domestic-signed.tgz'
 with Device('10.254.11.13', user='admin', password='3p1c.2010k', port=22) as dev:

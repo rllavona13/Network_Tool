0bbb416f54d316d8ff33cc42d99e1aff5947544f
Ramon Rivera <rllavona13@me.com>
2019-01-08 21:45:05 -0500

    new data structures

---
+++ Network_Tool/mikrotikServer/automate_routeros.py
@@ -0,0 +1,18 @@
+from napalm_ros import ros
+import napalm
+import napalm_ros
+
+router_ip = '192.168.1.1'
+router_port = 8728 # Use 8729 for api-ssl
+router_user = 'rrivera'
+router_pass = 'R4ym0nd12@'
+
+driver = napalm.get_network_driver('ros')
+
+print('Connecting to', router_ip, "on port", router_port, "as", router_user)
+
+device = driver(hostname=router_ip, username=router_user,
+                password=router_pass, optional_args={'port': router_port})
+
+print('Opening ...')
+device.open()

40f8bb38cb09c94e9c1af4255dc40e525b8c6926
Ramon Rivera <rllavona13@me.com>
2019-02-09 18:44:33 -0500

    useless edits

--- Network_Tool/mikrotikServer/automate_routeros.py
+++ Network_Tool/mikrotikServer/automate_routeros.py
@@ -2,12 +2,13 @@ from napalm_ros import ros
 import napalm
 import napalm_ros
 
+
 router_ip = '192.168.1.1'
 router_port = 8728 # Use 8729 for api-ssl
 router_user = 'rrivera'
 router_pass = 'R4ym0nd12@'
 
-driver = napalm.get_network_driver('ros')
+driver = napalm.get_network_driver('ROS')
 
 print('Connecting to', router_ip, "on port", router_port, "as", router_user)
 

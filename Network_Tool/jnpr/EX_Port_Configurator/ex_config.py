b61816f0fe99f71f594351b5b05cd609e1770071
Ramon Rivera <30485980+rllavona13@users.noreply.github.com>
2021-10-28 17:34:39 -0400

    Update ex_config.py

--- Network_Tool/jnpr/EX_Port_Configurator/ex_config.py
+++ Network_Tool/jnpr/EX_Port_Configurator/ex_config.py
@@ -1,3 +1,5 @@
+# This script is for configure Juniper Ports using Juniper NETCONF
+
 from pprint import pprint
 from jnpr.junos import Device
 import json
@@ -18,6 +20,19 @@ def facts():
 
     except Exception as ex:
         print("Error on %s : %s " % (host, ex))
+        
+def send_cmd():
+    
+    client1 = paramiko.SSHClient()
+    client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client1.connect(IP, username=username, password=password)
+    configure = client1.invoke_shell()
+    configure.send('configure')
+    configure.send('set interfaces ge-0/0/10 description "test"')
+    configure.send('show | compare')
+    configure.recv(1000)
+
+    client1.close()
 
 
 if __name__ == '__main__':

c7a93847674a10bbda0c19033862e43c7783968c
Ramon Rivera <30485980+rllavona13@users.noreply.github.com>
2021-10-28 17:29:26 -0400

    Update backup.py

--- Network_Tool/jnpr/Backup_Script/backup.py
+++ Network_Tool/jnpr/Backup_Script/backup.py
@@ -1,3 +1,7 @@
+# this script uses Juniper NETCONF in order to backup Juniper devices configuration.
+# Using Paramiko SCP module to upload the configurations files to a file server
+# It can backup from any Juniper Devices specified in the JSON File
+
 from jnpr.junos import Device
 from lxml import etree
 import json
@@ -18,14 +22,16 @@ def config_backup():
             dev = Device(user=config['user'], password=config['pass'], host=host, port='22')
             dev.open()
             data = dev.rpc.get_config(options={'format': 'set'})
-            # print(etree.tostring(data))
-            print('Creando Backup de %s con IP %s' % (dev.facts['fqdn'], host))
+            # uncomment next line for debug purposes
+            # print(etree.tostring(data)) 
+            print('Creatig configuration backup for %s with  IP address:  %s' % (dev.facts['fqdn'], host))
 
             file_name = dev.facts['fqdn']
             f = open(file_name, 'w')
             f.write(str(etree.tostring(data)))
             f.close()
 
+            
             local_file_path = ('/Users/rllavona/PycharmProjects/Network_Tool/Network_Tool/'
                                'jnpr/Backup_Script/' + dev.facts['fqdn'])
             remote_file_path = '/home/rrivera/backups/'
@@ -35,7 +41,7 @@ def config_backup():
             ssh.connect(hostname='10.255.6.38', username=config['username'], password=config['password'])
 
             scp = SCPClient(ssh.get_transport())
-            print('Subiendo file al server...')
+            print('Uploading backup files to the remote server...')
             scp.put(local_file_path, remote_file_path)
             scp.close()
             ssh.close()
@@ -44,27 +50,6 @@ def config_backup():
             print("Error on %s : %s " % (host, ex))
 
         logging.basicConfig(filename="junos_backup_error.log", level=logging.ERROR)
-""" 
-def send_cmd():
-    
-    client1 = paramiko.SSHClient()
 
-    client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    client1.connect(IP, username=username, password=password)
-
-    configure = client1.invoke_shell()
-
-    configure.send('configure')
-
-    configure.send('set interfaces ge-0/0/10 description "test"')
-
-    configure.send('show | compare')
-
-    print
-    configure.recv(1000)
-
-    client1.close()
-"""
 if __name__ == '__main__':
     config_backup()
